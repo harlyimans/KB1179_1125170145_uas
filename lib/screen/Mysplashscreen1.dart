2a8d1c3a9d355e6deb09d800fdfeeaf254f67d7a
harlyimans <119984227+harlyimans@users.noreply.github.com>
2025-11-02 09:18:07 +0700

    Create First Splashscreen page

---
+++ lib/screen/Mysplashscreen1.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MySplashscreen1 extends StatelessWidget {
+  const MySplashscreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
