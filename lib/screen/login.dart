dcc14a72260c46341ebc33438eac3a23198a3650
harlyimans <119984227+harlyimans@users.noreply.github.com>
2025-11-05 11:56:22 +0700

    Update Layout & Create Email Input

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,25 +6,50 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.lock_outline, size: 70, color: Colors.blue),
-          SizedBox(height: 20),
-          Text(
-            "Selamat Datang",
-            style: TextStyle(
-              fontSize: 30.0,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.lock_outline, size: 70, color: Colors.blue),
+            SizedBox(height: 20),
+            Text(
+              "Selamat Datang",
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          SizedBox(height: 5),
-          Text(
-            "Silahkan untuk login terlebih dahulu",
-            style: TextStyle(fontSize: 14.0, color: Colors.green),
-          ),
-        ],
+            SizedBox(height: 5),
+            Text(
+              "Silahkan untuk login terlebih dahulu",
+              style: TextStyle(fontSize: 14.0, color: Colors.green),
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                  hintText: "Silahkan Masukkan Alamat E-mail Anda",
+                  prefixIcon: Icon(Icons.email_outlined),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.green),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
